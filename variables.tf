8eede45321067a7f0030b46b8e60d21c16a68aba
Rodrigo Marliere <rmarliere@gmail.com>
2022-11-16 07:35:13 -0300

    dynamic blocks

--- variables.tf
+++ variables.tf
@@ -3,3 +3,36 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "cidr1" {
+  default = [
+      "10.0.0.0/8",
+    ]
+}
+
+variable "cidr2" {
+  default = [
+      "192.168.0.0/16",
+    ]
+}
+
+variable protocol_tcp {
+  default = "tcp"
+}
+
+variable "rules" {
+  default =[
+    {
+      from_port = 80
+      to_port   = 80
+    },
+    {
+      from_port = 22
+      to_port   = 22
+    },
+    {
+      from_port = 443
+      to_port   = 443
+    }
+  ]
+}
