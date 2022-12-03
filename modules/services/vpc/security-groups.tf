993a0bbe269d122b255676743b06008aeb832556
Rodrigo Marliere <rmarliere@gmail.com>
2022-12-03 07:55:33 -0300

    allowing outbound requests on security groups

--- modules/services/vpc/security-groups.tf
+++ modules/services/vpc/security-groups.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "node_group_one" {
       cidr_blocks = var.cidr1
     }
   }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "node_group_two" {
