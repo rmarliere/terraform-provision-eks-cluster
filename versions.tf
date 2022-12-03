ae5d568473dbfd1479fd5f90cb2cf082a7648087
Rodrigo Marliere <rmarliere@gmail.com>
2022-12-03 07:53:48 -0300

    adding state to remote s3 bucket

--- versions.tf
+++ versions.tf
@@ -7,5 +7,18 @@ terraform {
   }
 
   required_version = "1.3.5"
+
+    backend "s3" {
+
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    bucket         = "terraform-rmarliere"
+    key            = "test/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "table-rmarliere"
+    encrypt        = true
+
+  }
 }
 
